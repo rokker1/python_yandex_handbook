ea2d88dad0a8310d95bb5e327cb73355a2d94f3a
alexander@MaiBook-M Ubuntu 22.04 <darkriderification@yandex.ru>
2022-11-27 21:53:51 +0500

    q is done

--- 3.Collections/3.2Set_dict/q.py
+++ 3.Collections/3.2Set_dict/q.py
@@ -1,26 +1,39 @@
 db = {}
-db_2 = {}
+
+
+def print_second_frineds(sf):
+    for person in sorted(sf):
+        output_line = f"{person}: "
+
+        if sf[person]:
+            first = True
+            for second_friend in sorted(sf[person]):
+                if not first:
+                    output_line += ", "
+                first = False
+                output_line += second_friend
+        print(output_line)
+
+
 while (line := input()) != "":
     friends = line.split()
-    if not db.get(friends[0]):
-        db[friends[0]] = [friends[1]]
-    else:
+    if friends[0] in db:
         db[friends[0]].append(friends[1])
-    if not db_2.get(friends[1]):
-        db_2[friends[1]] = [friends[0]]
     else:
-        db_2[friends[1]].append(friends[0])
+        db[friends[0]] = [friends[1]]
+    if friends[1] in db:
+        db[friends[1]].append(friends[0])
+    else:
+        db[friends[1]] = [friends[0]]
 
-print(db)
-print(db_2)
+second_friends = {}
 
-for person, first_friends in db.items():
-    second_friends = set()
-    for first_friend in first_friends:
-        if first_friend in db:
-            for second_friend in db[first_friend]:
-                second_friends.add(second_friend)
+for person in db:
+    second_friends[person] = set()
+    for first_friend in db[person]:
+        for second_friend in db[first_friend]:
+            if person != second_friend:
+                second_friends[person].add(second_friend)
+    
 
-        for second_friend in db_2[first_friend]:
-            second_friends.add(second_friend)
-    print(person, second_friends)
+print_second_frineds(second_friends)
