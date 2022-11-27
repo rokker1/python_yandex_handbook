91770ae61b65fa1ec6e93bb997663064a88861e7
alexander@MaiBook-M Ubuntu 22.04 <darkriderification@yandex.ru>
2022-11-27 11:14:45 +0500

    a.py

---
+++ 3.Collections/3.2.Set_Dict/lesson_0.py
@@ -0,0 +1,3 @@
+vowels = {"а", "е", "ё", "и", "о"}
+empty_set = set()
+print(f"Длина пустого множества равна {len(empty_set)}")
---
+++ 3.Collections/3.2.Set_Dict/tasks.py/a.py
@@ -0,0 +1,6 @@
+line = input()
+chars = set(line)
+result_line = ""
+for char in chars:
+    result_line += char
+print(result_line)
