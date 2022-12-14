a1f64647cf9a7cfb87d6c8587a4a6ac982ece025
A.E.Zhavoronkov <darkriderification@yandex.ru>
2022-12-14 22:25:33 +0500

    e

--- 4.Functions/3.recursion_decorators/tasks/e.py
+++ 4.Functions/3.recursion_decorators/tasks/e.py
@@ -1,32 +1,34 @@
 _functions_queue = {}
-def result_accumulator(method):
+def result_accumulator(f):
     global _functions_queue
-    def answer(f):
+    def wrapper(*args, method = "accumulate", **kwargs):
+        f_name = f.__name__
+        if method == 'accumulate':
+            if f_name not in _functions_queue:
+                _functions_queue[f.__name__] = []
+            _functions_queue[f.__name__].append(f(*args, **kwargs))
+        
+        elif method == "drop":
+            if f_name not in _functions_queue:
+                res = []
+                res.append(f(*args, **kwargs))
+            else:
+                res = _functions_queue[f.__name__][:]
+                res.append(f(*args, **kwargs))
+                _functions_queue[f.__name__].clear()
+            return res
+    
+    return wrapper
 
-        def wrapper(*args, **kwargs):
-            f_name = f.__name__
-            if method == 'accumulate':
-                if f_name not in _functions_queue:
-                    _functions_queue[f.__name__] = []
-                _functions_queue[f.__name__].append(f(*args, **kwargs))
-            
-            elif method == "drop":
-                if f_name not in _functions_queue:
-                    res = []
-                else:
-                    res = _functions_queue[f.__name__]
-                return res
-        return wrapper
-    return answer
    
     
 
-@result_accumulator("accumulate")
+@result_accumulator
 def a_plus_b(a, b):
     return a + b
 
 
-print(a_plus_b(3, 5))
+print(a_plus_b(3, 5, method="accumulate"))
 print(a_plus_b(7, 9))
 print(a_plus_b(-3, 5, method="drop"))
 print(a_plus_b(1, -7))
